d8c2f6c149236783c76f7dfea943ccc06a8a560f
tesh254 <ewachira254@gmail.com>
2019-12-12 21:30:56 +0300

    proof of stake:
    - award peer with hude earnings made
    
    [status: done]

---
+++ proof_of_stake/main.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
+)
+
+type Block struct {
+	Index int
+	TimeStamp string
+	BPM int
+	Hash string
+	PrevHash string
+	Validator string
+}
+
+var Blockchain []Block
+
+var tempBlocks []Block
+
+var candidateBlocks = make(chan Block)
+
+var announcements = make(chan string)
+
+var mutex = &sync.Mutex{}
+
+var validators = make(map[string]int)
+
+func calculateHash(s string) string {
+	h := sha256.New()
+	h.Write([]byte(s))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func calculateBlockHash(block Block) string {
+	record := string(block.Index) + block.TimeStamp + string(block.BPM) + block.PrevHash
+	return calculateHash(record)	
+}
+
+func generateBlock(oldBlock Block, BPM int, address string) (Block, error) {
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.TimeStamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateBlockHash(newBlock)
+	newBlock.Validator = address
+
+	return newBlock, nil
+}
+
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index + 1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateBlockHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+
+	go func() {
+		for {
+			msg := <- announcements
+			io.WriteString(conn, msg)
+		}
+	}()
+
+	var address string
+
+	io.WriteString(conn, "Enter token balance: ")
+	scanBalance := bufio.NewScanner(conn)
+	for scanBalance.Scan() {
+		balance, err := strconv.Atoi(scanBalance.Text())
+		if err != nil {
+			log.Print("%v not a number: %v", scanBalance.Text(), err)
+			return
+		}
+
+		t := time.Now()
+		address = calculateHash(t.String())
+		validators[address] = balance
+		fmt.Println(validators)
+		break
+	}
+
+	io.WriteString(conn, "\nEnter a new BPM: ")
+
+	scanBPM := bufio.NewScanner(conn)
+
+	go func() {
+		for {
+			for scanBPM.Scan() {
+				bpm, err := strconv.Atoi(scanBPM.Text())
+
+				if err != nil {
+					log.Printf("%v not a number: %v", scanBPM.Text(), err)
+					delete(validators, address)
+					conn.Close()
+				}
+
+				mutex.Lock()
+				oldLastIndex := Blockchain[len(Blockchain)-1]
+				mutex.Unlock()
+
+				newBlock, err := generateBlock(oldLastIndex, bpm, address)
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+				if isBlockValid(newBlock, oldLastIndex) {
+					candidateBlocks <- newBlock
+				}
+
+				io.WriteString(conn, "\nEnter a new BPM: ")
+			}
+		}
+	}()
+
+	for {
+		time.Sleep(time.Minute)
+		mutex.Lock()
+		output, err := json.Marshal(Blockchain)
+		mutex.Unlock()
+		if err != nil {
+			log.Fatal(err)
+		}
+		io.WriteString(conn, string(output) + "\n")
+	}
+}
+
+func pickWinner() {
+	time.Sleep(30 * time.Second)
+	mutex.Lock()
+	temp := tempBlocks
+	mutex.Unlock()
+
+	lotteryPool := []string{}
+	if len(temp) > 0 {
+		OUTER:
+			for _, block := range temp {
+				for _, node := range lotteryPool{
+					if block.Validator == node {
+						continue OUTER
+					}
+				}
+
+				mutex.Lock()
+				setValidators := validators
+				mutex.Unlock()
+
+				k, ok := setValidators[block.Validator]
+				if ok {
+					for i := 0; i < k; i++ {
+						lotteryPool = append(lotteryPool, block.Validator)
+					}
+				}
+			}
+
+			s := rand.NewSource(time.Now().Unix())
+			r := rand.New(s)
+
+			lotteryWinner := lotteryPool[r.Intn(len(lotteryPool))]
+
+			for _, block := range temp {
+				if block.Validator == lotteryWinner {
+					mutex.Lock()
+					Blockchain = append(Blockchain, block)
+					mutex.Unlock()
+					for _ = range validators {
+						announcements <- "\nwinning validator: " + lotteryWinner + "\n"
+					}
+					break
+				}
+			}
+	}
+	mutex.Lock()
+	tempBlocks = []Block{}
+	mutex.Unlock()
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	t := time.Now()
+	genesisBlock := Block{}
+	genesisBlock = Block{0, t.String(), 0, calculateBlockHash(genesisBlock), "", ""}
+	spew.Dump(genesisBlock)
+	Blockchain = append(Blockchain, genesisBlock)
+
+
+	server, err := net.Listen("tcp", ":"+os.Getenv("ADDR"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer server.Close()
+
+	go func() {
+		for candidate := range candidateBlocks {
+			mutex.Lock()
+			tempBlocks = append(tempBlocks, candidate)
+			mutex.Unlock()
+		}
+	}()
+
+	go func() {
+		for {
+			pickWinner()
+		}
+	}()
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConn(conn)
+	}
+}
